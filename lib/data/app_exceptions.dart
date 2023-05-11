2e0467bdf007ffaaaf1b69ad4e1690f1fd44b3c2
Md Somad <mdsomad10@gmail.com>
2023-05-11 16:36:04 +0530

    Handle Exceptions Complete

---
+++ lib/data/app_exceptions.dart
@@ -0,0 +1,37 @@
+
+
+//TODO Create AppExceptions Class
+class AppExceptions implements Exception {
+   final _message;
+   final _prefix;
+
+  AppExceptions([this._message, this._prefix]);
+
+  String toString(){
+    return '$_prefix$_message';
+  }
+
+  
+}
+
+
+
+
+//TODO Create InternetException Class
+class InternetException extends AppExceptions{
+   InternetException([String? message]) : super(message,'No internet');
+}
+
+
+
+//TODO Create RequestTimeOutException Class
+class RequestTimeOutException extends AppExceptions{
+   RequestTimeOutException([String? message]) : super(message,'Request time out');
+}
+
+
+
+//TODO Create ServerException Class
+class ServerException extends AppExceptions{
+   ServerException([String? message]) : super(message,'Internal server error');
+}
---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => SsplashScreenState();
+}
+
+class SsplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
