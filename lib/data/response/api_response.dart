5af8103a1bf8d4353f85094ec9474564258b0c24
Md Somad <mdsomad10@gmail.com>
2023-05-11 17:05:25 +0530

    Handle Api Response Complete

--- lib/data/response/api_response.dart
+++ lib/data/response/api_response.dart
@@ -1,24 +1,10 @@
-# flutter_getx_mvvm_mvc
-
-A new Flutter project.
-- 1: Handle APIs Response
-
-- 1: Cteate enum
-```sh
-enum Status{LOADING,COMPLETED,ERROR}
-```
-
-
-
-- 2: Create generic class
-```sh
 import 'package:flutter_getx_mvvm_mvc/data/response/status.dart';
 
 
 //TODO Create generic class
 class ApiResponse<T> {
   Status? status;
-  T? data;          // <-- dynamic data provided
+  T? data;             //* <-- dynamic data provided
   String? message;
 
   ApiResponse(this.status,this.data,this.message);
@@ -38,5 +24,3 @@ class ApiResponse<T> {
   
   
 }
-
-```
